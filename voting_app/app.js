1e56199be63a950e594ef739a47edd374f4d227a
pearpages <pere@soms.cat>
2017-04-22 23:21:03 +0200

    static components

--- voting_app/app.js
+++ voting_app/app.js
@@ -2,11 +2,36 @@ const ProductList = React.createClass({
     render: function () {
         return (
             <div className="ui items">
-                Hello, friend! I am a basci React component.
+                <Product />
             </div>
         );
     }
 });
+
+const Product = React.createClass({
+    render: function () {
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src="images/products/default.png" alt="default"/>
+                </div>
+                <div className='middle aligned content'>
+                    <div className='midle aligned content'>
+                        <div className='description'>
+                            <a>For Knight</a>
+                            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                        </div>
+                        <div className='extra'>
+                            <span>Submitted by: </span>
+                            <img className='ui avatar image' src="images/avatars/avatar.png" alt="avatar"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
 
 ReactDOM.render(
     <ProductList />,
