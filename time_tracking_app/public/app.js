0a8196f8cfcb9c774d5acddaa13aae7e1b0e5618
pearpages <pere@soms.cat>
2017-04-25 18:14:01 +0200

    start/stop time_tracking

--- time_tracking_app/public/app.js
+++ time_tracking_app/public/app.js
@@ -41,6 +41,42 @@ const TimersDashboard = React.createClass({
             timers: this.state.timers.concat(t)
         });
     },
+    handleStartClick: function (timerId) {
+        this.startTimer(timerId);
+    },
+    handleStopClick: function (timerId) {
+        this.stopTimer(timerId);
+    },
+    startTimer: function (timerId) {
+        const now = Date.now();
+
+        this.setState({
+            timers: this.state.timers.map( (timer) => {
+                if (timer.id === timerId) {
+                    return Object.assign({}, timer, {runningSince: now})
+                } else {
+                    return timer;
+                }
+            })
+        });
+    },
+    stopTimer: function (timerId) {
+        const now = Date.now();
+
+        this.setState({
+            timers: this.state.timers.map( (timer) => {
+                if (timer.id === timerId) {
+                    const lastElapsed = now - timer.runningSince;
+                    return Object.assign({}, timer, {
+                        elapsed: timer.elapsed + lastElapsed,
+                        runningSince: null
+                    })
+                } else {
+                    return timer;
+                }
+            })
+        });
+    },
     updateTimer: function (attrs) {
         this.setState({
             timers: this.state.timers.map( (timer) => {
@@ -65,6 +101,8 @@ const TimersDashboard = React.createClass({
                         timers={this.state.timers}
                         onFormSubmit={this.handleEditFormSubmit}
                         onTrashClick={this.handleTrashClick}
+                        onStartClick={this.handleStartClick}
+                        onStopClick={this.handleStopClick}
                     />
                     <ToggleableTimerForm
                         onFormSubmit={this.handleCreateFormSubmit}
@@ -88,6 +126,8 @@ const EditableTimerList = React.createClass({
                 runningSince={timer.runningSince}
                 onFormSubmit={this.props.onFormSubmit}
                 onTrashClick={this.props.onTrashClick}
+                onStartClick={this.props.onStartClick}
+                onStopClick={this.props.onStopClick}
             />
         ));
         return (
@@ -143,6 +183,8 @@ const EditableTimer = React.createClass({
                     runningSince={this.props.runningSince}
                     onEditClick={this.handleEditClick}
                     onTrashClick={this.props.onTrashClick}
+                    onStartClick={this.props.onStartClick}
+                    onStopClick={this.props.onStopClick}
                 />
             )
         }
@@ -240,6 +282,12 @@ const Timer = React.createClass({
     handleTrashClick: function () {
         this.props.onTrashClick(this.props.id);
     },
+    handleStartClick: function () {
+        this.props.onStartClick(this.props.id);
+    },
+    handleStopClick: function () {
+        this.props.onStopClick(this.props.id);
+    },
     render: function () {
         const elapsedString = helpers.renderElapsedString(this.props.elapsed, this.props.runningSince);
         return (
@@ -264,9 +312,37 @@ const Timer = React.createClass({
                             <i className='trash icon'></i> </span>
                     </div>
                 </div>
-                <div className='ui bottom attached blue basic button'>Start</div>
+                <TimerActionButton
+                    timerIsRunning={!!this.props.runningSince}
+                    onStartClick={this.handleStartClick}
+                    onStopClick={this.handleStopClick}
+                />
             </div>
         );
+    }
+});
+
+const TimerActionButton = React.createClass({
+    render: function () {
+        if (this.props.timerIsRunning) {
+            return (
+                <div
+                    className='ui bottom attached red basic button'
+                    onClick={this.props.onStopClick}
+                >
+                    Stop
+                </div>
+            );
+        } else {
+            return (
+                <div
+                    className='ui bottom attached green basic button'
+                    onClick={this.props.onStartClick}
+                >
+                    Start
+                </div>
+            );
+        }
     }
 });
 
