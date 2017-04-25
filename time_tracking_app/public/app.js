e8afb71c6c9cadd23320d681381b51d98375c5e7
pearpages <pere@soms.cat>
2017-04-25 17:29:15 +0200

    inverse data flow, delete timers for time_tracking

--- time_tracking_app/public/app.js
+++ time_tracking_app/public/app.js
@@ -27,6 +27,14 @@ const TimersDashboard = React.createClass({
     handleEditFormSubmit: function (attrs) {
         this.updateTimer(attrs);
     },
+    handleTrashClick: function (timerId) {
+        this.deleteTimer(timerId);
+    },
+    deleteTimer: function (timerId) {
+        this.setState({
+            timers: this.state.timers.filter( t => t.id !== timerId)
+        });
+    },
     createTimer: function (timer) {
         const t = helpers.newTimer(timer);
         this.setState({
@@ -56,6 +64,7 @@ const TimersDashboard = React.createClass({
                     <EditableTimerList
                         timers={this.state.timers}
                         onFormSubmit={this.handleEditFormSubmit}
+                        onTrashClick={this.handleTrashClick}
                     />
                     <ToggleableTimerForm
                         onFormSubmit={this.handleCreateFormSubmit}
@@ -78,6 +87,7 @@ const EditableTimerList = React.createClass({
                 elapsed={timer.elapsed}
                 runningSince={timer.runningSince}
                 onFormSubmit={this.props.onFormSubmit}
+                onTrashClick={this.props.onTrashClick}
             />
         ));
         return (
@@ -132,6 +142,7 @@ const EditableTimer = React.createClass({
                     elapsed={this.props.elapsed}
                     runningSince={this.props.runningSince}
                     onEditClick={this.handleEditClick}
+                    onTrashClick={this.props.onTrashClick}
                 />
             )
         }
@@ -220,6 +231,9 @@ const ToggleableTimerForm = React.createClass({
 
 const Timer = React.createClass({
     // stateless
+    handleTrashClick: function () {
+        this.props.onTrashClick(this.props.id);
+    },
     render: function () {
         const elapsedString = helpers.renderElapsedString(this.props.elapsed);
         return (
@@ -239,6 +253,7 @@ const Timer = React.createClass({
                         </span>
                         <span
                             className='right floated trash icon'
+                            onClick={this.handleTrashClick}
                         >
                             <i className='trash icon'></i> </span>
                     </div>
