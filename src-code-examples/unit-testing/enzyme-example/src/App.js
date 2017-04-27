55879fc96635c223cc236d63c5b266e4e9a7bf6a
pearpages <pere@soms.cat>
2017-04-27 23:52:17 +0200

    app example for unit testing

---
+++ src-code-examples/unit-testing/enzyme-example/src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.onItemChange = this.onItemChange.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+    this.state = {
+      items: [],
+      item: ''
+    };
+
+  }
+
+  addItem(e) {
+    e.preventDefault();
+
+    this.setState({
+      items: this.state.items.concat(this.state.item),
+      item: ''
+    })
+  }
+
+  onItemChange(e) {
+    this.setState({ item: e.target.value });
+  }
+
+  render() {
+    const submitDisabled = this.state.item === '';
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <form
+          className='ui form'
+          onSubmit={this.addItem}
+        >
+          <div className='field'>
+            <input
+              className='prompt'
+              type='text'
+              placeholder='Add item...'
+              value={this.state.item}
+              onChange={this.onItemChange}
+            />
+          </div>
+          <button
+            className='ui button'
+            type='submit'
+            disabled={submitDisabled}
+          >
+            Add item
+          </button>
+        </form>
+
+        <br/>
+
+        <table style={{margin:'auto'}}>
+          <tbody>
+            {
+              this.state.items.map((item, idx) => (
+                <tr
+                  key={idx}
+                >
+                  <td>{item}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
