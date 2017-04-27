4bd59b5c7f680791fc47cd925ec1a261a4ae0d05
pearpages <pere@soms.cat>
2017-04-27 16:48:58 +0200

    testing library withouth a framework

---
+++ src-code-examples/unit-testing-without-framework/Modash.test.js
@@ -0,0 +1,12 @@
+import Modash from './Modash';
+
+const string = 'there was one catch, and that was CATCH-22';
+const actual = Modash.truncate(string, 19);
+const expected = 'there was one catch...';
+
+if (actual !== expected) {
+    console.log(
+        `[FAIL] Expected \`truncate()\` to return '${expected}', got '${actual}'`);
+} else {
+    console.log('[PASS] `truncate()`.');
+}
