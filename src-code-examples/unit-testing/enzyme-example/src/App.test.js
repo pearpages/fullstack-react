4dec9dfbcac6dcc7bb6f36e0ab98f9ffeaac0796
pearpages <pere@soms.cat>
2017-04-28 01:51:40 +0200

    testing with enzyme the generated html

--- src-code-examples/unit-testing/enzyme-example/src/App.test.js
+++ src-code-examples/unit-testing/enzyme-example/src/App.test.js
@@ -4,7 +4,31 @@ import App from './App';
 
 describe('App', function () {
   it(`should have the 'th' "Items"`, function () {
-    const wrapper = shallow(<App/>);
+    const wrapper = shallow(<App />);
     expect(wrapper.contains(<th>Items</th>)).toBe(true);
   });
+
+  it(`should have a button with "Add Item"`, function () {
+    const wrapper = shallow(<App />);
+    expect(wrapper.containsMatchingElement(<button>Add item</button>)).toBe(true);
+  });
+
+  it('`button` should be disabled', () => {
+    const wrapper = shallow(
+      <App />);
+    const button = wrapper.find('button').first();
+    expect(
+      button.props().disabled
+    ).toBe(true);
+  });
+
+  it('should have an `input` element', () => {
+    const wrapper = shallow(
+      <App />);
+    expect(
+      wrapper.containsMatchingElement(
+        <input />)
+    ).toBe(true);
+  });
+
 });
