aedcc45eb023d087d4beb4263e00714cbd2436fc
pearpages <pere@soms.cat>
2017-04-28 01:58:53 +0200

    beforeEach jest

--- src-code-examples/unit-testing/enzyme-example/src/App.test.js
+++ src-code-examples/unit-testing/enzyme-example/src/App.test.js
@@ -3,19 +3,23 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', function () {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />);
+  });
+
   it(`should have the 'th' "Items"`, function () {
-    const wrapper = shallow(<App />);
     expect(wrapper.contains(<th>Items</th>)).toBe(true);
   });
 
   it(`should have a button with "Add Item"`, function () {
-    const wrapper = shallow(<App />);
     expect(wrapper.containsMatchingElement(<button>Add item</button>)).toBe(true);
   });
 
   it('`button` should be disabled', () => {
-    const wrapper = shallow(
-      <App />);
     const button = wrapper.find('button').first();
     expect(
       button.props().disabled
@@ -23,8 +27,6 @@ describe('App', function () {
   });
 
   it('should have an `input` element', () => {
-    const wrapper = shallow(
-      <App />);
     expect(
       wrapper.containsMatchingElement(
         <input />)
