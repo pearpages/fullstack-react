18aa79a5cd7da881188c2bb7c9ff50b8ff9607d3
pearpages <pere@soms.cat>
2017-05-08 18:52:26 +0200

    combine reducers

--- src/redux/chat-with-redux-library/public/app.js
+++ src/redux/chat-with-redux-library/public/app.js
@@ -1,32 +1,13 @@
-function reducer(state, action) {
-  return {
-    activeThreadId: activeThreadIdReducer(state.activeThreadId, action),
-    threads: threadsReducer(state.threads, action)
-  }
-}
+const reducer = Redux.combineReducers({
+  activeThreadId: activeThreadIdReducer,
+  threads: threadsReducer,
+});
 
-function messagesReducer(state = [], action) {
-  switch (action.type) {
-    case 'ADD_MESSAGE': {
-      const newMessage = {
-        text: action.text,
-        timestamp: Date.now(),
-        id: uuid.v4()
-      };
-      return state.concat(newMessage);
-    }
-    case 'DELETE_MESSAGE': {
-      const messageIndex = state.findIndex((m) => m.id === action.id);
-      return [
-        ...state.slice(0, messageIndex),
-        ...state.slice(
-          messageIndex + 1, state.length
-        )
-      ];
-    }
-    default: {
-      return state;
-    }
+function activeThreadIdReducer(state = '1-fca2', action) {
+  if (action.type === 'OPEN_THREAD') {
+    return action.id;
+  } else {
+    return state;
   }
 }
 
@@ -39,46 +20,43 @@ function findThreadIndex(threads, action) {
     }
     case 'DELETE_MESSAGE': {
       return threads.findIndex(
-        (t) => t.messages.find((m) => (m.id === action.id))
+        (t) => t.messages.find((m) => (
+          m.id === action.id
+        ))
       );
     }
   }
 }
 
-function threadsReducer(state = [ // Two threads in state
+function threadsReducer(state = [
   {
-    id: '1-fca2', // hardcoded pseudo-UUID title: 'Buzz Aldrin',
+    id: '1-fca2',
     title: 'Buzz Aldrin',
-    messages: [
-      {
-        // This thread starts with a single message already
-        text: 'Twelve minutes to ignition.',
-        timestamp: Date.now(),
-        id: uuid.v4(),
-      },
-    ]
+    messages: messagesReducer(undefined, {}),
   },
   {
     id: '2-be91',
     title: 'Michael Collins',
-    messages: []
-  }
+    messages: messagesReducer(undefined, {}),
+  },
 ], action) {
   switch (action.type) {
     case 'ADD_MESSAGE':
     case 'DELETE_MESSAGE': {
-      const threadIndex = state.findThreadIndex(state, action);
+      const threadIndex = findThreadIndex(state, action);
+
       const oldThread = state[threadIndex];
       const newThread = {
         ...oldThread,
-        messages: messagesReducer(oldThread.messages, action)
+        messages: messagesReducer(oldThread.messages, action),
       };
+
       return [
         ...state.slice(0, threadIndex),
         newThread,
         ...state.slice(
           threadIndex + 1, state.length
-        )
+        ),
       ];
     }
     default: {
@@ -87,40 +65,32 @@ function threadsReducer(state = [ // Two threads in state
   }
 }
 
-function activeThreadIdReducer(state = '1-fca2', action) {
-  if (action.type === 'OPEN_THREAD') {
-    return action.id;
-  } else {
-    return state;
+function messagesReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_MESSAGE': {
+      const newMessage = {
+        text: action.text,
+        timestamp: Date.now(),
+        id: uuid.v4(),
+      };
+      return state.concat(newMessage);
+    }
+    case 'DELETE_MESSAGE': {
+      const messageIndex = state.findIndex((m) => m.id === action.id);
+      return [
+        ...state.slice(0, messageIndex),
+        ...state.slice(
+          messageIndex + 1, state.length
+        ),
+      ];
+    }
+    default: {
+      return state;
+    }
   }
 }
 
 const store = Redux.createStore(reducer);
-
-const ThreadTabs = React.createClass({
-  handleClick: function (id) {
-    store.dispatch({
-      type: 'OPEN_THREAD',
-      id: id
-    })
-  },
-  render: function () {
-    const tabs = this.props.tabs.map((tab, index) => (
-      <div
-        key={index}
-        className={tab.active ? 'active item' : 'item'}
-        onClick={() => this.handleClick(tab.id)}
-      >
-        {tab.title}
-      </div>
-    ));
-    return (
-      <div className='ui top attached tabular menu'>
-        {tabs}
-      </div>
-    );
-  }
-});
 
 const App = React.createClass({
   componentDidMount: function () {
@@ -132,11 +102,13 @@ const App = React.createClass({
     const threads = state.threads;
     const activeThread = threads.find((t) => t.id === activeThreadId);
 
-    const tabs = threads.map(t => ({ // a 'tab' object
-      title: t.title,
-      active: t.id === activeThreadId,
-      id: t.id
-    }));
+    const tabs = threads.map(t => (
+      {
+        title: t.title,
+        active: t.id === activeThreadId,
+        id: t.id,
+      }
+    ));
 
     return (
       <div className='ui segment'>
@@ -147,12 +119,68 @@ const App = React.createClass({
   },
 });
 
+const Tabs = (props) => (
+  <div className='ui top attached tabular menu'>
+    {
+      props.tabs.map((tab, index) => (
+        <div
+          key={index}
+          className={tab.active ? 'active item' : 'item'}
+          onClick={() => props.onClick(tab.id)}
+        >
+          {tab.title}
+        </div>
+      ))
+    }
+  </div>
+);
+
+const ThreadTabs = React.createClass({
+  render: function () {
+    return (
+      <Tabs
+        tabs={this.props.tabs}
+        onClick={(id) => (
+          store.dispatch({
+            type: 'OPEN_THREAD',
+            id: id,
+          })
+        )}
+      />
+    );
+  },
+});
+
+const TextFieldSubmit = (props) => {
+  let input;
+
+  return (
+    <div className='ui input'>
+      <input
+        ref={node => input = node}
+        type='text'
+      >
+      </input>
+      <button
+        onClick={() => {
+          props.onSubmit(input.value);
+          input.value = '';
+        }}
+        className='ui primary button'
+        type='submit'
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
 const MessageInput = React.createClass({
   handleSubmit: function () {
     store.dispatch({
       type: 'ADD_MESSAGE',
       text: this.refs.messageInput.value,
-      threadId: this.props.threadId
+      threadId: this.props.threadId,
     });
     this.refs.messageInput.value = '';
   },
@@ -170,7 +198,7 @@ const MessageInput = React.createClass({
           type='submit'
         >
           Submit
-      </button>
+        </button>
       </div>
     );
   },
@@ -190,8 +218,10 @@ const Thread = React.createClass({
         key={index}
         onClick={() => this.handleClick(message.id)}
       >
-        {message.text}
-        <span className='metadata'>@{message.timestamp}</span>
+        <div className='text'>
+          {message.text}
+          <span className='metadata'>@{message.timestamp}</span>
+        </div>
       </div>
     ));
     return (
